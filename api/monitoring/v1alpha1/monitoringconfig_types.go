52c2517fa2abe4eaa4cb92d122d013d7e3028d21
Chi Fujii <chi.fujii@solanica.io>
2026-03-20 13:08:30 +0900

    do not include conditions now

--- api/monitoring/v1alpha1/monitoringconfig_types.go
+++ api/monitoring/v1alpha1/monitoringconfig_types.go
@@ -54,6 +54,9 @@ type PMMConfig struct {
 }
 
 // MonitoringConfigStatus defines the observed state of MonitoringConfig.
+//
+// FIXME: Add []metav1.Condition to represent the current state of the MonitoringConfig resource. Currently adding Conditions fails due to
+// producing duplicated Go constants in generated code.
 type MonitoringConfigStatus struct {
 	// InUse is a flag that indicates if any Instance uses the monitoring config.
 	// +kubebuilder:default=false
@@ -62,20 +65,6 @@ type MonitoringConfigStatus struct {
 	LastObservedGeneration int64 `json:"lastObservedGeneration,omitempty"`
 	// PMMServerVersion shows PMM server version.
 	PMMServerVersion PMMServerVersion `json:"pmmServerVersion,omitempty"`
-
-	// conditions represent the current state of the MonitoringConfig resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
