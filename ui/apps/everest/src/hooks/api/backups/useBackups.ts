7980ce999606d21532748b769e5eb442df361d9a
Aditya Pimpalkar <adipimpalkar@gmail.com>
2026-04-24 18:02:56 +0530

    fix: lint & copyright checks
    Signed-off-by: Aditya Pimpalkar <adipimpalkar@gmail.com>

--- ui/apps/everest/src/hooks/api/backups/useBackups.ts
+++ ui/apps/everest/src/hooks/api/backups/useBackups.ts
@@ -1,3 +1,17 @@
+// Copyright (C) 2026 The OpenEverest Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {
   useMutation,
   UseMutationOptions,
@@ -43,19 +57,19 @@ export const useDbBackups = (
     queryFn: () => getBackupsFn(dbClusterName, namespace),
     select: canRead
       ? ({ items = [] }) =>
-        items.map(
-          ({ metadata: { name }, status, spec: { backupStorageName } }) => ({
-            name,
-            created: status?.created,
-            completed: status?.completed,
-            state: status
-              ? mapBackupState(status?.state)
-              : BackupStatus.UNKNOWN,
-            size: status?.size,
-            dbClusterName,
-            backupStorageName,
-          })
-        )
+          items.map(
+            ({ metadata: { name }, status, spec: { backupStorageName } }) => ({
+              name,
+              created: status?.created,
+              completed: status?.completed,
+              state: status
+                ? mapBackupState(status?.state)
+                : BackupStatus.UNKNOWN,
+              size: status?.size,
+              dbClusterName,
+              backupStorageName,
+            })
+          )
       : () => [],
     ...options,
     enabled: (options?.enabled ?? true) && canRead,
