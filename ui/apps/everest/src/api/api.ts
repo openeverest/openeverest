da8f00a2193bd784d53aa9437db39e2d39650bbb
ashnaaseth2325-oss <ashnaaseth2325@gmail.com>
2026-05-04 16:25:35 +0000

    chore: apply correct copyright headers
    Signed-off-by: ashnaaseth2325-oss <ashnaaseth2325@gmail.com>

--- ui/apps/everest/src/api/api.ts
+++ ui/apps/everest/src/api/api.ts
@@ -1,5 +1,4 @@
-
-// Copyright (c) 2026 Percona LLC
+// Copyright (C) 2026 The OpenEverest Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,6 +6,14 @@
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Copyright (c) 2024 Percona LLC
+// Licensed under the Apache License, Version 2.0
 
 import axios, { AxiosError } from 'axios';
 import { enqueueSnackbar } from 'notistack';
