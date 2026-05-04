685278ac1a98356211533d09b33197ecd37044d1
ashnaaseth2325-oss <ashnaaseth2325@gmail.com>
2026-05-04 16:19:54 +0000

    chore: fix copyright header format
    Signed-off-by: ashnaaseth2325-oss <ashnaaseth2325@gmail.com>

--- ui/apps/everest/src/api/api.ts
+++ ui/apps/everest/src/api/api.ts
@@ -1,8 +1,5 @@
 
 // Copyright (c) 2026 Percona LLC
-// Licensed under the Apache License, Version 2.0
-// everest
-// Copyright (C) 2023 Percona LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,11 +7,7 @@
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 import axios, { AxiosError } from 'axios';
 import { enqueueSnackbar } from 'notistack';
 
