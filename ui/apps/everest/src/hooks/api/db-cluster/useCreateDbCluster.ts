6277331241fd394fec165b16deb4beea969cd931
ashnaaseth2325-oss <ashnaaseth2325@gmail.com>
2026-05-06 22:41:11 +0000

    fix: scope db credentials cache by namespace; reactive rbac route guard
    Signed-off-by: ashnaaseth2325-oss <ashnaaseth2325@gmail.com>

--- ui/apps/everest/src/hooks/api/db-cluster/useCreateDbCluster.ts
+++ ui/apps/everest/src/hooks/api/db-cluster/useCreateDbCluster.ts
@@ -1,5 +1,6 @@
 // everest
 // Copyright (C) 2023 Percona LLC
+// Copyright (C) 2026 The OpenEverest Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -211,7 +212,7 @@ export const useDbClusterCredentials = (
   );
 
   return useQuery<GetDbClusterCredentialsPayload, unknown, ClusterCredentials>({
-    queryKey: ['cluster-credentials', dbClusterName],
+    queryKey: ['cluster-credentials', namespace, dbClusterName],
     queryFn: () => getDbClusterCredentialsFn(dbClusterName, namespace),
     select: canReadCredentials
       ? (creds) => creds
