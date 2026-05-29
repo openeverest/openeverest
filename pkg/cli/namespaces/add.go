4198bdd5421db8a70ab9e3a74c62a9e44c7e3a86
Chi Fujii <chi.fujii@solanica.io>
2026-05-29 10:46:35 +0900

    remove installing resources removed from helm
    Signed-off-by: Chi Fujii <chi.fujii@solanica.io>

--- pkg/cli/namespaces/add.go
+++ pkg/cli/namespaces/add.go
@@ -35,8 +35,8 @@ import (
 	"github.com/openeverest/openeverest/v2/pkg/common"
 	"github.com/openeverest/openeverest/v2/pkg/kubernetes"
 	"github.com/openeverest/openeverest/v2/pkg/output"
-	. "github.com/openeverest/openeverest/v2/pkg/utils/must" //nolint:revive,stylecheck
 	"github.com/openeverest/openeverest/v2/pkg/version"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type (
@@ -408,23 +408,23 @@ func (n *NamespaceAdder) provisionDBNamespace(
 	if err != nil {
 		return err
 	}
-	values := Must(helmutils.MergeVals(n.getValues(), nil))
-	installer := helm.Installer{
-		ReleaseName:            namespace,
-		ReleaseNamespace:       namespace,
-		Values:                 values,
-		CreateReleaseNamespace: !nsExists,
+	
+	// TODO: The everest-db-namespace chart has been removed from the helm-charts repository.
+	// Database namespace provisioning is now handled differently or is no longer needed as a separate chart.
+	// This functionality needs to be reimplemented or removed entirely.
+	n.l.Infof("Skipping DB namespace Helm chart installation for namespace %s (chart no longer exists)", namespace)
+	
+	// Create namespace if it doesn't exist
+	if !nsExists {
+		n.l.Infof("Creating namespace %s", namespace)
+		ns := &corev1.Namespace{}
+		ns.Name = namespace
+		if _, err := n.kubeClient.CreateNamespace(ctx, ns); err != nil {
+			return fmt.Errorf("could not create namespace: %w", err)
+		}
 	}
-	if err := installer.Init(n.cfg.KubeconfigPath, helm.ChartOptions{
-		Directory: cliutils.DBNamespaceSubChartPath(n.cfg.HelmConfig.ChartDir),
-		URL:       n.cfg.HelmConfig.RepoURL,
-		Name:      helm.EverestDBNamespaceChartName,
-		Version:   version,
-	}); err != nil {
-		return fmt.Errorf("could not initialize Helm installer: %w", err)
-	}
-	n.l.Info("Installing DB namespace Helm chart in namespace ", namespace)
-	return installer.Install(ctx)
+	
+	return nil
 }
 
 func (n *NamespaceAdder) validateNamespaceUpdate(ctx context.Context, namespace string) error {
