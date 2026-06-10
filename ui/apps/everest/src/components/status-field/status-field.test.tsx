83aa9b7c670a875b017f1367a1a64483f94398ac
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-06-10 17:41:56 +0530

    test: address StatusField review feedback

--- ui/apps/everest/src/components/status-field/status-field.test.tsx
+++ ui/apps/everest/src/components/status-field/status-field.test.tsx
@@ -1,3 +1,17 @@
+// Copyright (C) 2026 The OpenEverest Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { render, screen } from '@testing-library/react';
 import { TestWrapper } from 'utils/test';
 import StatusField from './status-field';
@@ -5,19 +19,12 @@ import { BaseStatus } from './status-field.types';
 import { STATUS_TO_ICON } from './status-field.utils';
 
 describe('STATUS_TO_ICON', () => {
-  it.each<BaseStatus>([
-    'success',
-    'error',
-    'pending',
-    'paused',
-    'unknown',
-    'deleting',
-    'creating',
-    'upgrading',
-    'importing',
-  ])('defines an icon for "%s"', (status) => {
-    expect(STATUS_TO_ICON[status]).toBeDefined();
-  });
+  it.each(Object.keys(STATUS_TO_ICON) as BaseStatus[])(
+    'defines an icon for "%s"',
+    (status) => {
+      expect(STATUS_TO_ICON[status]).toBeDefined();
+    }
+  );
 });
 
 describe('StatusField', () => {
