1d6e670d4b4146d70748250e1a00917faff67610
StepanovPlaton <mail2platon@yandex.ru>
2026-06-20 12:50:23 +0400

    Remove circular dependency (#2334) and add strict eslint rule
    Signed-off-by: StepanovPlaton <mail2platon@yandex.ru>

--- ui/packages/ui-lib/.eslintrc.cjs
+++ ui/packages/ui-lib/.eslintrc.cjs
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
   extends: ['@percona/eslint-config-react', 'plugin:storybook/recommended'],
+  rules: {
+    // Prevent internal ui-lib components from importing through the package root barrel.
+    // Use direct relative paths (e.g. '../../../labeled-content') instead.
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: '../../..',
+            message:
+              "Do not import from the ui-lib package root barrel inside src/. Use a direct relative path (e.g. '../../../labeled-content') instead.",
+          },
+          {
+            name: '../../../index',
+            message:
+              "Do not import from the ui-lib package root barrel inside src/. Use a direct relative path (e.g. '../../../labeled-content') instead.",
+          },
+        ],
+      },
+    ],
+  },
 };
