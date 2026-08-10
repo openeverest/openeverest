a33a41c82a88d51accfd574f9100beded244d60f
Bhupesh Nandardhane <nandardhanebhupesh@gmail.com>
2026-08-10 22:30:54 +0545

    fix(common): fix PSMBDOperator typo to PSMDBOperator
    Fixes #2860
    
    Signed-off-by: Bhupesh Nandardhane <nandardhanebhupesh@gmail.com>

--- pkg/common/version_test.go
+++ pkg/common/version_test.go
@@ -1,10 +1,26 @@
+// Copyright (C) 2026 The OpenEverest Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
 import (
 	"testing"
 
+	versionpb "github.com/Percona-Lab/percona-version-service/versionpb"
 	goversion "github.com/hashicorp/go-version"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCompareVersions(t *testing.T) {
@@ -36,4 +52,31 @@ func TestCheckConstraints(t *testing.T) {
 	assert.True(t, CheckConstraint("1.2.1-rc1", "~> 1.2.0"))
 	assert.False(t, CheckConstraint("1.3.1-rc1", "~> 1.2.0"))
 	assert.True(t, CheckConstraint("1.3.1-rc1", "> 1.2.0"))
+}
+
+func TestNewSupportedVersion(t *testing.T) {
+	t.Parallel()
+
+	meta := &versionpb.MetadataVersion{
+		Supported: map[string]string{
+			"cli":           ">= 1.0.0",
+			"olm":           ">= 0.20.0",
+			"catalog":       ">= 1.0.0",
+			"kubernetes":    ">= 1.22.0",
+			"pgOperator":    ">= 2.4.0",
+			"pxcOperator":   ">= 1.10.0",
+			"psmdbOperator": ">= 1.15.0",
+		},
+	}
+
+	supVer, err := NewSupportedVersion(meta)
+	require.NoError(t, err)
+
+	assert.True(t, supVer.Cli.Check(goversion.Must(goversion.NewVersion("1.0.0"))))
+	assert.True(t, supVer.Olm.Check(goversion.Must(goversion.NewVersion("0.20.0"))))
+	assert.True(t, supVer.Catalog.Check(goversion.Must(goversion.NewVersion("1.0.0"))))
+	assert.True(t, supVer.Kubernetes.Check(goversion.Must(goversion.NewVersion("1.22.0"))))
+	assert.True(t, supVer.PGOperator.Check(goversion.Must(goversion.NewVersion("2.4.0"))))
+	assert.True(t, supVer.PXCOperator.Check(goversion.Must(goversion.NewVersion("1.10.0"))))
+	assert.True(t, supVer.PSMDBOperator.Check(goversion.Must(goversion.NewVersion("1.15.0"))))
 }
